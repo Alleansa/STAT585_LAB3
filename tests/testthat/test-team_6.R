ca9f2dc6175d37580c80d106ec2e6ca9827697e0
zahrakhoshmanesh <zkh@iastate.edu>
2019-03-21 01:40:39 -0500

    add warning in team_6 solution

--- tests/testthat/test-team_6.R
+++ tests/testthat/test-team_6.R
@@ -1,5 +1,6 @@
 context("test-team_6")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("creating map for a country works", {
+  expect_warning(Team_6(tolerance = 'a'),"argument is not numeric or logical: returning NA")
+  expect_equal(is.data.frame(Team_6(tolerance = 0.1)),TRUE) 
 })
